968c1310c9c7538db6e8f9c28b433bef98171bd7
Emilio Martínez Rivero <erivero@emergya.com>
2022-09-20 17:51:27 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 /**
+ * @doc
  * a function that receives a string as parameter and returns the severity level according to the comparison of the string parameter 
  * with the values:"max", "min", "sum", "sum2", "occur", "avg", "max" if the string argument is null or undefined
  */

5f7401813391fc3c274a42bae93d7a96ada709f1
Emilio Martínez Rivero <erivero@emergya.com>
2022-09-20 17:59:13 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 // a function that receives a string as parameter and returns the severity level according to the comparison of the string parameter with the values:"max", "min", "sum", "sum2", "occur", "avg", "max" if the string argument is null or undefined
-export function builder(string: string): AggregateOperation {
+function builder(string: string): AggregateOperation {
   return string;
 }

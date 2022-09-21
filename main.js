4deb9c0edd2b1199d80b781961a45ea35bdf295a
Emilio Martínez Rivero <erivero@emergya.com>
2022-09-21 12:43:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-/**
- * a function that receives a string as a parameter and 
- * returns an array of strings resulting from splitting the string with 
- * the pattern "/"" and filtering the empty strings.
- */
+// a function that receives a string as a parameter and returns an array of strings resulting from splitting the string with the pattern "/"" and filtering the empty strings
 const getBreadCrumbs = (path) => {
   const ArrayOfPaths = path.split('/');
   return ArrayOfPaths.filter(() => path.trim() !== '');

3ec1f74b5090fe50b4a4c072e82be98b801d435b
Emilio Martínez Rivero <erivero@emergya.com>
2022-09-21 12:29:05 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * a function that receives a string as a parameter and 
+ * returns an array of strings resulting from splitting the string with 
+ * the pattern "/"" and filtering the empty strings.
+ */
+const getBreadCrumbs = (path) => {
+  const ArrayOfPaths = path.split('/');
+  return ArrayOfPaths.filter(() => path.trim() !== '');
+};

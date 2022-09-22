4adaec126a56ffd95918b56d7d4248c2f99fb43a
Emilio Martínez Rivero <erivero@emergya.com>
2022-09-22 11:51:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ const getBreadCrumbs = (path) => {
   const ArrayOfPaths = path.split('/');
   return ArrayOfPaths.filter(() => path.trim() !== '');
 };
+
+// a function that receives a string as a parameter and returns an array of strings resulting from splitting the string with the pattern "/"" and filtering the empty strings
+export const getBreadCrumbs = (path) => {
+  const ArrayOfPaths = path.split('/');
+  return ArrayOfPaths.filter(() => path.trim() !== '');
+};
+
+// a function that receives a string as a parameter and returns an array of strings resulting from splitting the string with the pattern "/"" and filtering the empty strings
+function getBreadCrumbs(path) {
+  const ArrayOfPaths = path.split('/');
+  return ArrayOfPaths.filter(() => path.trim() !== '');
+};
+
+
+// a function that receives a string as a parameter and returns an array of strings resulting from splitting the string with the pattern "/"" and filtering the empty strings
+export function getBreadCrumbs(path) {
+  const ArrayOfPaths = path.split('/');
+  return ArrayOfPaths.filter(() => path.trim() !== '');
+};
